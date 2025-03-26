1173d4c5bb5e3ca1dbec6d7442c8bbc6584a2940
Francis Quezon <aisukezon@gmail.com>
2025-03-26 19:01:14 +1100

    Moved index.ts to inside src and then modified tsconfig and package.json to add the views folder

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import express, { Application } from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
 
-import { router as MainRouter } from './src/routers/router';
-import { router as ApiRouter } from './src/routers/apiRouter';
-import { appErrorHandler } from './src/middleware/appErrorMiddleware';
+import { router as MainRouter } from './routers/router';
+import { router as ApiRouter } from './routers/apiRouter';
+import { appErrorHandler } from './middleware/appErrorMiddleware';
 
 //For env File 
 dotenv.config();
@@ -13,7 +13,7 @@ const app: Application = express();
 const port = process.env.PORT || 8000;
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "src/views")); // Folder for templates
+app.set("views", path.join(__dirname, "views")); // Folder for templates
 
 app.use("/", MainRouter);
 app.use("/api", ApiRouter);
